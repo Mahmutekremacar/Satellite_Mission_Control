a17cfc35ff8d1cfc51f859fb489ed45abb42e79e
Mahmutekremacar <mahmutekremacar@gmail.com>
2025-06-23 20:00:41 +0200

    demo full

--- java/com/example/hellofx/Main.java
+++ java/com/example/hellofx/Main.java
@@ -9,11 +9,11 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/hellofx/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Satellite Mission Control");
         stage.setScene(scene);
         stage.setHeight(600);
-        stage.setWidth(600);
+        stage.setWidth(500);
         stage.show();
     }
 
